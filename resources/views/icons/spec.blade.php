c7361b2b5ecd403419f673e02d50b10fa8a35766
Rusty Fausak <rustyfausak@gmail.com>
2016-11-06 01:54:58 -0500

    hack for file case

--- resources/views/icons/spec.blade.php
+++ resources/views/icons/spec.blade.php
@@ -1,1 +1,1 @@
-<img class="wowicon wowicon-spec" src="{{ url('img/icons/specs/' . ucfirst(strtolower(str_replace(' ', '', $role))) . '-' . ucfirst(strtolower(str_replace(' ', '', $spec))) . '.jpg') }}" title="{{ $spec . ' ' . $role }}">
+<img class="wowicon wowicon-spec" src="{{ url('img/icons/specs/' . strtolower(str_replace(' ', '', $role)) . '-' . strtolower(str_replace(' ', '', $spec)) . '.jpg') }}" title="{{ $spec . ' ' . $role }}">
